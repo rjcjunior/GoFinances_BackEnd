00f4739c9d354a70512ee8675ac1dd1547dab01d
rjcjunior <rjcjunior@hotmail.com>
2020-09-21 11:10:20 -0300

    Rotas para deletar transações

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 
-// import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
@@ -26,9 +26,14 @@ transactionsRouter.post('/', async (request, response) => {
   return response.json(transaction);
 });
 
-// transactionsRouter.delete('/:id', async (request, response) => {
-//   // TODO
-// });
+transactionsRouter.delete('/:id', async (request, response) => {
+  const transactionsService = new DeleteTransactionService();
+  const { id } = request.params;
+
+  await transactionsService.execute(id);
+
+  return response.status(204).json();
+});
 
 // transactionsRouter.post('/import', async (request, response) => {
 //   // TODO
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transactionsExists = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!transactionsExists) {
+      throw new AppError('Transaction id not valid', 400);
+    }
+
+    transactionRepository.delete(transactionsExists.id);
   }
 }
 
