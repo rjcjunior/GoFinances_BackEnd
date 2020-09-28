afd74423a73242b3c860beeb4800a52cd19f8524
rjcjunior <rjcjunior@hotmail.com>
2020-09-28 00:25:55 -0300

    Ajustes para o desafio de reactjs

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -5,7 +5,9 @@ import {
   Column,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
 } from 'typeorm';
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
@@ -20,6 +22,9 @@ class Category {
 
   @UpdateDateColumn()
   updated_at: Date;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 }
 
 export default Category;
